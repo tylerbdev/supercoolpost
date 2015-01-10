9d1b123f4a971c9085a320504466701ef4ae5e77
tylerbdev <tyler@tylerbarton.me>
2015-01-09 16:26:07 -0800

    wip  cms project

---
+++ header.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>"Welcome to SuperCoolPost!"</title>
+<link rel="stylesheet" href="css/style.css">
+<style>
+body {
+	margin:0px;
+	background-color:#E1E1E1;
+}
+</style>
+<table class="header_table">
+	<tr>
+	<td style="width:70%; padding-left:9px"><a href="index.php"><img src="images/logo.png" height="100px"></img></a></td>
+	<td style="width:30%">Login</td>
+	</tr>
+</table>
